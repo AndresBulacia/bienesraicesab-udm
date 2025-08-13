29e5846b4067ae308a89303a0786c34c75dbb9a2
Andrés Bulacia <andresbulacia040@gmail.com>
2025-08-13 18:54:48 +0200

    CRUD template page created

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -5,7 +5,51 @@
 ?>
 
     <main class="contenedor seccion">
-        <h1>Titulo Página</h1>
+        <h1>Crear</h1>
+
+        
+        <form class="formulario">
+            <fieldset>
+                <legend>Información General</legend>
+
+                <label for="titulo">Titulo:</label>
+                <input type="text" id="titulo" placeholder="Titulo Propiedad">
+                
+                <label for="preco">Precio:</label>
+                <input type="number" id="precio" placeholder="Precio Propiedad">
+                
+                <label for="imagen">Imagen:</label>
+                <input type="file" id="imagen" accept="image/jpeg, image/png">
+
+                <label for="descripcion">Descripción:</label>
+                <textarea id="descripcion"></textarea>
+            </fieldset>
+
+            <fieldset>
+                <legend>Información Propiedad</legend>
+
+                <label for="habitaciones">Habitaciones:</label>
+                <input type="number" id="habitaciones" placeholder="Ej: 3" min="1" max="5">
+
+                <label for="wc">Baños:</label>
+                <input type="number" id="wc" placeholder="Ej: 3" min="1" max="3">
+                
+                <label for="estacionamiento">Estacionamiento:</label>
+                <input type="number" id="estacionamiento" placeholder="Ej: 3" min="1" max="5">
+            </fieldset>
+            
+            <fieldset>
+                <legend>Vendedor</legend>
+                
+                <select name="" id="">
+                    <option value="1">Juan</option>
+                    <option value="2">Andrés</option>
+                </select>
+            </fieldset>
+            
+            <input type="submit" value="Crear Propiedad" class="boton boton-verde">
+        </form>
+        <a href="/admin/index.php" class="boton boton-verde">Volver</a>
     </main>
 
     <?php 
--- includes/templates/header.php
+++ includes/templates/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bienes Raices</title>
-    <link rel="stylesheet" href="build/css/app.css">
+    <link rel="stylesheet" href="/build/css/app.css">
 </head>
 <body>
     
@@ -14,15 +14,15 @@
             <!-- Apertura Barra -->
             <div class="barra">
                 <a href="index.php">
-                    <img src="build/img/logo.svg" alt="logo de la web">
+                    <img src="/build/img/logo.svg" alt="logo de la web">
                 </a>
 
                 <div class="mobile-menu">
-                    <img src="build/img/barras.svg" alt="icono menú resposive">
+                    <img src="/build/img/barras.svg" alt="icono menú resposive">
                 </div>
 
                 <div class="derecha">
-                    <img class="dark-mode-boton" src="build/img/dark-mode.svg" alt="dark-mode icon">
+                    <img class="dark-mode-boton" src="/build/img/dark-mode.svg" alt="dark-mode icon">
                     <nav class="navegacion">
                         <a href="nosotros.php">Nosotros</a>
                         <a href="anuncios.php">Anuncios</a>
